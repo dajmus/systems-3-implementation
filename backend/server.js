e2f2d9cf6869277a44d415af93b99cb86804d047
Dimitrije Glamočanin <89211102@student.upr.si>
2026-08-11 21:10:47 +0200

    starting the project, setting up database connection

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 3001; app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => res.send('Backend running'));
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
